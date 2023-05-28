13286f4ad4902fea391767cce0ac39e37bcfba1b
meshi <mehhi@yandex.ru>
2023-05-28 16:03:44 +0300

    react

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,13 +1,16 @@
 import s from "./Chat.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { socket } from "../utils/socket";
 import debug from "../debugManager/DebugManager";
 
 const Chat = () => {
     const [isConnected, setIsConnected] = useState(socket.connected);
+    const [chatMsg, setChatMsg] = useState('');
+    const [messages, setMessages] = useState([]);
 
     const sendMsg = (event) => {
         event.preventDefault();
+        socket.emit("chat message", chatMsg);
     };
 
     useEffect(() => {
@@ -21,8 +24,14 @@ const Chat = () => {
             setIsConnected(false);
         }
 
+        function onMessage(data) {
+            debug.log(`get message from backend: ${data}`);
+            setMessages([...messages, data]);
+        }
+
         socket.on('connect', onConnect);
         socket.on('disconnect', onDisconnect);
+        socket.on('chat message', onMessage);
 
         return () => {
             socket.off('connect', onConnect);
@@ -32,9 +41,13 @@ const Chat = () => {
 
     return(
         <>
-            <ul className={s.messages}></ul>
+            <ul className={s.messages}>
+                {messages.map((message) => <li>{message}</li>)}
+            </ul>
             <form className={s.chat} action="">
-                <input className={s.chat__input} autoComplete="off" />
+                <input className={s.chat__input} autoComplete="off" onChange={(event) => {
+                    setChatMsg(event.target.value);
+                }}/>
                 <button onClick={sendMsg}>Send</button>
             </form>
         </>
